31cc7dc75f9fa4a8d407a370dd87fb1a9908c657
jvmafra <joao.mafra@ccc.ufcg.edu.br>
2018-08-03 00:55:00 -0300

    ajustand thread

--- kvstore/src/main/java/sd/kvstore/KeyValueServer.java
+++ kvstore/src/main/java/sd/kvstore/KeyValueServer.java
@@ -40,7 +40,7 @@ public class KeyValueServer {
     CopycatServer server = CopycatServer.builder(address)
         .withStateMachine(KeyValueStore::new)
         .withTransport(NettyTransport.builder().withThreads(4).build())
-        .withStorage(Storage.builder().withDirectory(new File("/local/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
+        .withStorage(Storage.builder().withDirectory(new File("/home/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
         .build();
 
     server.bootstrap().thenAccept(srvr -> LOG.info(srvr + " has bootstrapped a cluster")).join();
@@ -52,39 +52,39 @@ public class KeyValueServer {
     CopycatServer server1 = CopycatServer.builder(server1Address)
             .withStateMachine(KeyValueStore::new)
             .withTransport(NettyTransport.builder().withThreads(4).build())
-            .withStorage(Storage.builder().withDirectory(new File("/local/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
+            .withStorage(Storage.builder().withDirectory(new File("/home/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
             .build();
     
     server1.join(clusterAddress).thenAccept(srvr -> System.out.println(srvr + " has joined the cluster"));
     
-//    Address server2Address = new Address("localhost", 5002);
-//    CopycatServer server2 = CopycatServer.builder(server2Address)
-//            .withStateMachine(KeyValueStore::new)
-//            .withTransport(NettyTransport.builder().withThreads(4).build())
-//            .withStorage(Storage.builder().withDirectory(new File("/local/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
-//            .build();
-//    
-//    server2.join(clusterAddress).thenAccept(srvr -> System.out.println(srvr + " has joined the cluster"));
-//    
-//    Address server3Address = new Address("localhost", 5003);
-//    CopycatServer server3 = CopycatServer.builder(server3Address)
-//            .withStateMachine(KeyValueStore::new)
-//            .withTransport(NettyTransport.builder().withThreads(4).build())
-//            .withStorage(Storage.builder().withDirectory(new File("/local/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
-//            .build();
-//    
-//    server3.join(clusterAddress).thenAccept(srvr -> System.out.println(srvr + " has joined the cluster"));
-//    
-//    Address server4Address = new Address("localhost", 5004);
-//    CopycatServer server4 = CopycatServer.builder(server4Address)
-//            .withStateMachine(KeyValueStore::new)
-//            .withTransport(NettyTransport.builder().withThreads(4).build())
-//            .withStorage(Storage.builder().withDirectory(new File("/local/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
-//            .build();
-//    
-//    server4.join(clusterAddress).thenAccept(srvr -> System.out.println(srvr + " has joined the cluster"));
-//    
-//    
+    Address server2Address = new Address("localhost", 5002);
+    CopycatServer server2 = CopycatServer.builder(server2Address)
+            .withStateMachine(KeyValueStore::new)
+            .withTransport(NettyTransport.builder().withThreads(4).build())
+            .withStorage(Storage.builder().withDirectory(new File("/home/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
+            .build();
+    
+    server2.join(clusterAddress).thenAccept(srvr -> System.out.println(srvr + " has joined the cluster"));
+    
+    Address server3Address = new Address("localhost", 5003);
+    CopycatServer server3 = CopycatServer.builder(server3Address)
+            .withStateMachine(KeyValueStore::new)
+            .withTransport(NettyTransport.builder().withThreads(4).build())
+            .withStorage(Storage.builder().withDirectory(new File("/home/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
+            .build();
+    
+    server3.join(clusterAddress).thenAccept(srvr -> System.out.println(srvr + " has joined the cluster"));
+    
+    Address server4Address = new Address("localhost", 5004);
+    CopycatServer server4 = CopycatServer.builder(server4Address)
+            .withStateMachine(KeyValueStore::new)
+            .withTransport(NettyTransport.builder().withThreads(4).build())
+            .withStorage(Storage.builder().withDirectory(new File("/home/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs")).withStorageLevel(StorageLevel.DISK).build())
+            .build();
+    
+    server4.join(clusterAddress).thenAccept(srvr -> System.out.println(srvr + " has joined the cluster"));
+    
+    
 //    Address server5Address = new Address("localhost", 5005);
 //    CopycatServer server5 = CopycatServer.builder(server5Address)
 //            .withStateMachine(KeyValueStore::new)
@@ -107,63 +107,62 @@ public class KeyValueServer {
     client.connect(clusterAddress).join();
 
     // Submit operations
-   
-    for (int i = 0; i <= 1; i++) {
-    	client.submit(new Put(i, "test")).thenRun(() -> LOG.info("Put succeeded")).join();
-    }
     
+    client.submit(new Put(1, "Hello world!"));
    
     Thread thread = new Thread(){
-    	boolean isBlocked = false;;
-        Calendar data1;
-        Calendar data2;
+    	
+        int cont = 0;
+        
         public void run(){
           while (true) {
-        	  try {
-        		  //thenAccept(result -> LOG.info("get"))
-        		  client.submit(new Get(1)).join();
-        		  if (isBlocked) {
-        			  isBlocked = false;
-        			  data2 = new GregorianCalendar();
-        			  System.out.println("TEMPO DE RECUPERACAO:");
-        			  System.out.println(data2.getTimeInMillis() - data1.getTimeInMillis());
-        			  System.out.println(data2.getTimeInMillis());
-        			  System.out.println(data1.getTimeInMillis());
-        		  }
-			} catch (Exception e) {
-				if (isBlocked == false) {
-					data1 = new GregorianCalendar();
-				}
-				isBlocked = true;
-				
-			}
+        	  cont += 1;
+        	  
+        	  if (cont == 10) {
+        		   server.leave();
+        		   
+        	  } else {
+        		  try {
+      				Thread.sleep(500);
+      			} catch (InterruptedException e1) {
+      				e1.printStackTrace();
+      			}
+        		
+        		
+        		try {
+        			   CompletableFuture<Void> f = client.submit(new Get(1)).thenAccept(result -> System.out.println("Result is: " + result));
+        			   
+        			   if (f.get() != null) {
+            			   System.out.println(f.get());
+        			   }
+        		} catch (Exception e) {
+    				System.out.println(e.getMessage());
+    				
+    			}
+        		  
+        	  }
         	  
           }
         }
-     };
-     
-    System.out.println(server.cluster().leader().address().port());
-    
+     };   
     thread.start();
-    
-    server.leave();
    
-    //Install listeners
-    client.submit(new Listen()).thenRun(() -> LOG.info("listener registered")).join();
-    client.onEvent("put", (EntryEvent event) -> LOG.info("put event received for: " + event.key));
-    client.onEvent("delete", (EntryEvent event) -> LOG.info("delete event received for: " + event.key));
-    client.onEvent("expire", (EntryEvent event) -> LOG.info("TTL expired event received for: " + event.key));
-
-    // Submit operations to trigger event listeners
-    client.submit(new Put("foo", "bar")).join();
-    client.submit(new Get("foo")).join();
-    client.submit(new Delete("foo")).join();
-    client.submit(new PutWithTtl("foo", "bar", 1000)).join();
+//    //Install listeners
+//    client.submit(new Listen()).thenRun(() -> LOG.info("listener registered")).join();
+//    client.onEvent("put", (EntryEvent event) -> LOG.info("put event received for: " + event.key));
+//    client.onEvent("delete", (EntryEvent event) -> LOG.info("delete event received for: " + event.key));
+//    client.onEvent("expire", (EntryEvent event) -> LOG.info("TTL expired event received for: " + event.key));
+//
+//    // Submit operations to trigger event listeners
+//    client.submit(new Put("foo", "bar")).join();
+//    client.submit(new Get("foo")).join();
+//    client.submit(new Delete("foo")).join();
+//    client.submit(new PutWithTtl("foo", "bar", 1000)).join();
 
   }
 
   private static void deleteLogs() throws Throwable {
-    Files.walkFileTree(new File("/local/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs").toPath(), new SimpleFileVisitor<Path>() {
+    Files.walkFileTree(new File("/home/mafra/workspace/distributed-datastore/kvstore/src/main/java/sd/kvstore/logs").toPath(), new SimpleFileVisitor<Path>() {
       @Override
       public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException {
         Files.delete(file);
